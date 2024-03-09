12a4d97662b065aa8337e30c391f13eacc487786
Pak <pakapongm@gmail.com>
2024-03-09 03:16:32 -0800

    refactored pictures as well as texts in the projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
+import HackerGrove from "../../Assets/Projects/project 2.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import SkyScan from "../../Assets/Projects/project1.png";
 
 function Projects() {
   return (
@@ -23,7 +23,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Academa"
-              description="With OpenAI, this application generates flashcards for the user to use to study and also has a chatbot to help you answer any questions you may have to help"
+              description="With OpenAI, this app will generate flashcards for the user to use to study and readily provides an AI chatbot to guide and answer any questions you may have about the topic."
               ghLink="https://github.com/Pak547/academa2"
             // demoLink=""
             />
@@ -31,20 +31,20 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={HackerGrove}
               isBlog={false}
               title="HackerGrove"
-              description="A habit tracking application where developers can track their time spent on projects, and learning new languages."
+              description="Habitually reinforces a healthy behaviors for software devs who want to track their time spent on projects, or learning new languages."
               ghLink="https://github.com/Pak547/hacker-grove"
               demoLink="hacker-grove-215ce88624e3.herokuapp.com/"
             />
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={SkyScan}
               isBlog={false}
               title="SkyScan"
-              description="Utilizing API calls to openWeather, this application allows users to search for weather conditions in any city in America as well as any current events happening in that city."
+              description="Utilizing API calls to openWeather, this application enables users to search for weather conditions in any location in America as well as purchasable events happening in that city."
               ghLink="https://github.com/Pak547/surprise-me-daily"
               demoLink="https://pak547.github.io/surprise-me-daily/"
             />
