961743b9c7d83a4a60c3997c01a5f40a40abc64b
Pak <pakapongm@gmail.com>
2024-03-25 10:25:29 -0700

    added new picture of myself editted

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/picofme.jpg";
+import myImg from "../../Assets/PAKedited.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
