8a255d7aec72f5e4a6fa224460264abd41bc13b7
Pak <pakapongm@gmail.com>
2024-03-30 23:07:20 -0700

    trying to get form to work functionally

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -7,7 +7,6 @@ export default function ContactForm() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
   console.log(errors);
-
   return (
     <Container>
       <Col>
@@ -22,7 +21,7 @@ export default function ContactForm() {
       <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
       </Row>
       <Row>
-      <textarea placeholder="Message" {...register("Message", {required: true})} />
+      <textarea placeholder="Message" {...register("Message", {required: true,})} />
       </Row>
       <input type="submit" />
     </form>
