4c43302f5fc694cd893e2578cfa3bb1a17ca38e0
Pak <pakapongm@gmail.com>
2024-03-30 20:10:14 -0700

    changing about.js to include short story

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Who is <strong className="purple">PAK?</strong>
+              About <strong className="purple">Pak</strong>
             </h1>
             <Aboutcard />
           </Col>
