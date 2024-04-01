fb64661c2d362cf48aafa7e924fae47b340a12ee
Pak <pakapongm@gmail.com>
2024-03-31 21:21:02 -0700

    adjusted spacing

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -4,68 +4,75 @@ import { Col, Container, Row } from 'react-bootstrap';
 function ContactForm() {
   const [state, handleSubmit] = useForm("mgegnpkb");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    return <p>Thanks for joining!</p>;
   }
   return (
     <Container>
-        <Col>
-      <form onSubmit={handleSubmit}>
-        <Row>
-      <label htmlFor="name">
-        Full Name
-      </label>
-      <input
-        id="name"
-        type="name" 
-        name="name"
-        />
-      <ValidationError
-        prefix="Name" 
-        field="name"
-        errors={state.errors}
-        />
-      </Row>
-      {""}
-      <Row>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-        />
-        </Row>
-        <Row>
-      <label htmlFor="message">
-        Message
-      </label>
-      <textarea
-        id="message"
-        name="message"
-        />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-        />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-        </Row>
-    </form>
-</Col>
+      <Col>
+        <form onSubmit={handleSubmit}>
+          <Row style={{ marginBottom: "15px" }}>
+            <input
+              placeholder='Full Name'
+              id="name"
+              type="name"
+              name="name"
+            />
+            <ValidationError
+              prefix="Name"
+              field="name"
+              errors={state.errors}
+            />
+          </Row>
+          <Row style={{ marginBottom: "15px" }}>
+            <input
+              placeholder='Email Address'
+              id="email"
+              type="email"
+              name="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </Row>
+          <Row style={{ marginBottom: "15px" }}>
+            <input
+              placeholder='Subject'
+              id="subject"
+              name="subject"
+            />
+            <ValidationError
+              prefix="Subject"
+              field="subject"
+              errors={state.errors}
+            />
+          </Row>
+            <Row style={{ marginBottom: "15px" }}>
+              <textarea
+                placeholder='Message'
+                id="message"
+                name="message"
+              />
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
+            </Row>
+          <Row>
+            <button type="submit" disabled={state.submitting}>
+              Submit
+            </button>
+          </Row>
+        </form>
+      </Col>
     </Container>
   );
 }
 function App() {
   return (
     <ContactForm />
-    );
+  );
 }
 export default App;
