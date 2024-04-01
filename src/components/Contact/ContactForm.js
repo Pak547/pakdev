f43399d9adcbf3ee19ea0102b4dea76be281c7f4
Pak <pakapongm@gmail.com>
2024-03-31 21:10:06 -0700

    new version of contact form using new api

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,28 +1,71 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Container, Row, Col } from "react-bootstrap";
-
-
-export default function ContactForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
+import { useForm, ValidationError } from '@formspree/react';
+import { Col, Container, Row } from 'react-bootstrap';
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mgegnpkb");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
   return (
     <Container>
-      <Col>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Row>
-      <input type="text" placeholder="Name" {...register("name", {required: true, maxLength: 30})} />
+        <Col>
+      <form onSubmit={handleSubmit}>
+        <Row>
+      <label htmlFor="name">
+        Full Name
+      </label>
+      <input
+        id="name"
+        type="name" 
+        name="name"
+        />
+      <ValidationError
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+        />
       </Row>
+      {""}
       <Row>
-      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      </Row>
-      <Row>
-      <textarea placeholder="Message" {...register("Message", {required: true,})} />
-      </Row>
-      <input type="submit" />
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+        />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+        />
+        </Row>
+        <Row>
+      <label htmlFor="message">
+        Message
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+        />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+        </Row>
     </form>
-      </Col>
+</Col>
     </Container>
   );
 }
+function App() {
+  return (
+    <ContactForm />
+    );
+}
+export default App;
